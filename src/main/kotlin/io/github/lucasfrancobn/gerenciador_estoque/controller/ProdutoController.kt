2275015e5bc60ab11cd9f58e3da2e11a80b41f65
lucasfrancobn <lucasfbnavarro@gmail.com>
2025-08-09 21:08:05 -0300

    feat: adicionando anotação @PreAuthorize nos endpoints

--- src/main/kotlin/io/github/lucasfrancobn/gerenciador_estoque/controller/ProdutoController.kt
+++ src/main/kotlin/io/github/lucasfrancobn/gerenciador_estoque/controller/ProdutoController.kt
@@ -5,6 +5,7 @@ import io.github.lucasfrancobn.gerenciador_estoque.dto.CadastrarProdutoRequest
 import io.github.lucasfrancobn.gerenciador_estoque.dto.ProdutoResponse
 import io.github.lucasfrancobn.gerenciador_estoque.service.ProdutoService
 import org.springframework.http.ResponseEntity
+import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -23,6 +24,7 @@ class ProdutoController(
     val service: ProdutoService
 ) {
     @PostMapping
+    @PreAuthorize("hasAnyRole('OPERADOR', 'ADMIN')")
     fun cadastrarProduto(@RequestBody request: CadastrarProdutoRequest): ResponseEntity<ProdutoResponse> {
         val produtoCadastrado = service.cadastrarProduto(request)
         val uri = ServletUriComponentsBuilder
@@ -35,24 +37,28 @@ class ProdutoController(
     }
 
     @GetMapping
+    @PreAuthorize("hasAnyRole('OPERADOR', 'ADMIN')")
     fun obterTodosProdutos(): ResponseEntity<List<ProdutoResponse>> {
         val produtos = service.buscarTodosProdutos()
         return ResponseEntity.ok(produtos)
     }
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasAnyRole('OPERADOR', 'ADMIN')")
     fun obterProdutoPorId(@PathVariable id: UUID): ResponseEntity<ProdutoResponse> {
         val produtoBuscado = service.buscarProdutoPorId(id)
         return ResponseEntity.ok(produtoBuscado)
     }
 
     @PutMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     fun  atualizarProduto(@PathVariable id: UUID, @RequestBody request: AtualizarProdutoRequest): ResponseEntity<ProdutoResponse> {
         val produtoBuscado = service.atualizarProduto(id, request)
         return ResponseEntity.ok(produtoBuscado)
     }
 
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     fun deletarProduto(@PathVariable id: UUID): ResponseEntity<Void> {
         service.deletarProduto(id);
         return ResponseEntity.noContent().build()
